4e174da998f0e51b83f4c2c24bcf361721523952
vikasr <vikasr@winjit.com>
2024-12-20 11:57:30 +0530

    commented lod code and added new logic for data ingestion

--- src/cnnClassifier/components/data_ingestion.py
+++ src/cnnClassifier/components/data_ingestion.py
@@ -9,7 +9,7 @@ from cnnClassifier import logger
 from cnnClassifier.utils import get_size # to check the file size
 
 
-'''class DataIngestion:
+class DataIngestion:
     def __init__(self, config: DataIngestionConfig):
         self.config = config
 
@@ -74,40 +74,40 @@ from cnnClassifier.utils import get_size # to check the file size
             print(f"Error: The file {self.config.local_data_file} is not a valid zip file.")
         except Exception as e:
             print(f"Error during unzip and clean process: {e}")
-'''
 
-class DataIngestion:
-    def __init__(self, config: DataIngestionConfig):
-        self.config = config
 
-    def download_file(self):
-        logger.info("Trying to download file...")
-        if not os.path.exists(self.config.local_data_file):
-            logger.info("Download started...")
-            filename, headers = request.urlretrieve(
-                url=self.config.source_URL,
-                filename=self.config.local_data_file
-            )
-            logger.info(f"{filename} download! with following info: \n{headers}")
-        else:
-            logger.info(f"File already exists of size: {get_size(Path(self.config.local_data_file))}")        
+# class DataIngestion:
+#     def __init__(self, config: DataIngestionConfig):
+#         self.config = config
 
-    def _get_updated_list_of_files(self, list_of_files):
-        return [f for f in list_of_files if f.endswith(".jpg") and ("Cat" in f or "Dog" in f)]
+#     def download_file(self):
+#         logger.info("Trying to download file...")
+#         if not os.path.exists(self.config.local_data_file):
+#             logger.info("Download started...")
+#             filename, headers = request.urlretrieve(
+#                 url=self.config.source_URL,
+#                 filename=self.config.local_data_file
+#             )
+#             logger.info(f"{filename} download! with following info: \n{headers}")
+#         else:
+#             logger.info(f"File already exists of size: {get_size(Path(self.config.local_data_file))}")        
 
-    def _preprocess(self, zf: ZipFile, f: str, working_dir: str):
-        target_filepath = os.path.join(working_dir, f)
-        if not os.path.exists(target_filepath):
-            zf.extract(f, working_dir)
+#     def _get_updated_list_of_files(self, list_of_files):
+#         return [f for f in list_of_files if f.endswith(".jpg") and ("Cat" in f or "Dog" in f)]
+
+#     def _preprocess(self, zf: ZipFile, f: str, working_dir: str):
+#         target_filepath = os.path.join(working_dir, f)
+#         if not os.path.exists(target_filepath):
+#             zf.extract(f, working_dir)
         
-        if os.path.getsize(target_filepath) == 0:
-            logger.info(f"removing file:{target_filepath} of size: {get_size(Path(target_filepath))}")
-            os.remove(target_filepath)
+#         if os.path.getsize(target_filepath) == 0:
+#             logger.info(f"removing file:{target_filepath} of size: {get_size(Path(target_filepath))}")
+#             os.remove(target_filepath)
 
-    def unzip_and_clean(self):
-        logger.info(f"unzipping file and removing unawanted files")
-        with ZipFile(file=self.config.local_data_file, mode="r") as zf:
-            list_of_files = zf.namelist()
-            updated_list_of_files = self._get_updated_list_of_files(list_of_files)
-            for f in tqdm(updated_list_of_files): # type: ignore
-                self._preprocess(zf, f, self.config.unzip_dir) # type: ignore
+#     def unzip_and_clean(self):
+#         logger.info(f"unzipping file and removing unawanted files")
+#         with ZipFile(file=self.config.local_data_file, mode="r") as zf:
+#             list_of_files = zf.namelist()
+#             updated_list_of_files = self._get_updated_list_of_files(list_of_files)
+#             for f in tqdm(updated_list_of_files): # type: ignore
+#                 self._preprocess(zf, f, self.config.unzip_dir) # type: ignore
